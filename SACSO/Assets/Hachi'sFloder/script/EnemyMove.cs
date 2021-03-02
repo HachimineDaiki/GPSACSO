8ee9c0be3261cca10374008ad9285490cc5136f1
HachimineDaiki <daiki-hachimine-19h@kbc.ac.jp>
2021-03-02 15:49:52 +0900

    Stage Wall

--- SACSO/Assets/Hachi'sFloder/script/EnemyMove.cs
+++ SACSO/Assets/Hachi'sFloder/script/EnemyMove.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectMove : MonoBehaviour
+public class EnemyMove : MonoBehaviour
 {
     // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
