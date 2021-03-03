7c36a60282cb27ad4e2bac023150d444a923ed4e
DRQE <https://github.com/DRQE/mamimumema>
2021-03-03 13:46:14 +0900

    現時点（敗北者

--- SACSO/Assets/Ooshiro/Script/Pushdoor.cs
+++ SACSO/Assets/Ooshiro/Script/Pushdoor.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class OshidashiDoor : MonoBehaviour
+public class Pushdoor : MonoBehaviour
 {
+    private void PushEnemy()
+    {
+        //エネミーの吹き飛ばし処理を書くところ
+    }
+    private void Update()
+    {
+        
+    }
     void OnTriggerEnter(Collider other)
     {
-        //if()ドアステータスを見るところ
         if (other.gameObject.tag == "Player"){//(今は仮にPlayer  //そのうちエネミーを入れるところ
-            //this.gameObject.GetComponent<Renderer>().material.color = new Color(1, 0, 0, 1);
-            //エネミーの吹き飛ばし処理を書くところ
+            this.gameObject.GetComponent<Renderer>().material.color = new Color(0, 0, 1, 1);
         }
     }
+    
     //void OnTriggerExit(Collider other)
     //{
     //    if (other.gameObject.tag == "Player"){
