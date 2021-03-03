bf10b163b461f6791a67aed97dba7acaa4dc5043
DRQE <https://github.com/DRQE/mamimumema>
2021-03-03 11:06:39 +0900

    一旦PUSH

---
+++ SACSO/Assets/Ooshiro/Script/OshidashiDoor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class OshidashiDoor : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        //if()ドアステータスを見るところ
+        if (other.gameObject.tag == "Player"){//(今は仮にPlayer  //そのうちエネミーを入れるところ
+            //this.gameObject.GetComponent<Renderer>().material.color = new Color(1, 0, 0, 1);
+            //エネミーの吹き飛ばし処理を書くところ
+        }
+    }
+    //void OnTriggerExit(Collider other)
+    //{
+    //    if (other.gameObject.tag == "Player"){
+    //        this.gameObject.GetComponent<Renderer>().material.color = new Color(0, 0, 1, 1);
+    //    }
+    //}Exit処理いる？いらなくね？
+}
---
+++ SACSO/Assets/Ooshiro/Script/PlayerMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 2.0f;
+    private Rigidbody rB;
+    void Start()
+    {
+        rB = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        rB.AddForce(x * speed, 0, z * speed, ForceMode.Impulse);
+    }
+}
