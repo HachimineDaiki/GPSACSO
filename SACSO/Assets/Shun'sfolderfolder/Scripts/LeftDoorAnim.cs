47c1549b045d79d73aad770b7e1a7a213ace1d6a
Shun_Nakada <shun-nakada-19h@stu.kbc.ac.jp>
2021-03-03 11:18:50 +0900

    ドアを新しく作りました(BaseDoor)

---
+++ SACSO/Assets/Shun'sfolderfolder/Scripts/LeftDoorAnim.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftDoorAnim : MonoBehaviour
+{
+    //　ドアのアニメーター
+    private Animator animator;
+    private bool NextState;
+
+    void Start()
+    {
+        // 親のAnimatorを取得
+        animator = transform.parent.GetComponent<Animator>();
+        NextState = true;
+    }
+
+    /// <summary>
+    /// 自動ドア検知エリアに入った時
+    /// </summary>
+    /// <param name="other"></param>
+	private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            // アニメーションパラメータをNextStateにする。(ドアを開閉する) true:開く false:閉まる
+            animator.SetBool("Open", NextState);
+
+            //次のステートに変更
+            if (NextState) NextState = false;
+            else NextState = true;
+        }
+        
+    }
+
+
+}
--- SACSO/Assets/Shun'sfolderfolder/Scripts/RollDoor.cs
+++ SACSO/Assets/Shun'sfolderfolder/Scripts/RollDoor.cs
@@ -6,17 +6,16 @@ public class RollDoor : MonoBehaviour
 {
     [SerializeField]DoorType type;
     [SerializeField]DoorStatus doorstatus;
-    private int Cnt;
-    [SerializeField]private float Movequantity;
 
     [SerializeField]float time;
 
-    float distance = 90f;       //回転量
+    private float distance;       //回転量
 
     private bool active;
     // Use this for initialization
     void Start()
     {
+        distance = 90f;
         List<GameObject> list = ChildGet.GetAll(gameObject);
         foreach (GameObject obj in list)
         {
@@ -29,32 +28,30 @@ public class RollDoor : MonoBehaviour
                 type = DoorType.Right;
             }
         }
+
         //ローテーションのYを左開きなら減算/右開きなら加算
         if (type == DoorType.Left)
         {
             distance *= -1;
         }
-
-        if (doorstatus == DoorStatus.Open)
-        {
-            distance *= -1;
-        }
         doorstatus = DoorStatus.Close;
         active = false;
-        Cnt = 0;
-        Movequantity = 0;
         time = 0f;
     }
 
     private void Update()
     {
+        Debug.Log(time);
         if (Input.GetKeyDown(KeyCode.F))
         {
             if (!active) active = true;
         }
-        if (active) RotationD(type);
-        float angle = Mathf.LerpAngle(transform.rotation.y, distance, time);
-        transform.eulerAngles = new Vector3(0, angle, 0);
+        if (active)
+        {
+            RotationD(type);
+            float angle = Mathf.LerpAngle(transform.rotation.y, transform.rotation.y+distance, time);
+            transform.eulerAngles = new Vector3(0, angle, 0);
+        }
     }
 
     void RotationD(DoorType type)
@@ -70,12 +67,6 @@ public class RollDoor : MonoBehaviour
         {
             time += Time.deltaTime * 2;
         }
-        //Debug.Log(time);
-        //Vector3 vector3 = new Vector3(transform.rotation.x, transform.rotation.y + ( distance / 10), transform.rotation.z);
-        //transform.Rotate(vector3);
-
-        Movequantity += Mathf.Abs(distance / 10);
-        //if(Movequantity >= 90f)
         if(time >= 1f || time <= 0)
         {
             if(time > 1f)
@@ -87,7 +78,6 @@ public class RollDoor : MonoBehaviour
                 time = 0;
             }
             active = false;
-            Movequantity = 0;
             if (doorstatus == DoorStatus.Close)
             {
                 doorstatus = DoorStatus.Open;
