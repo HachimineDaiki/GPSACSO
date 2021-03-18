1d04d27bcc90219dd817b0b220c38966eab2faa6
Shun_Nakada <shun-nakada-19h@stu.kbc.ac.jp>
2021-03-18 18:42:29 +0900

    壊した処理

---
+++ SACSO/Assets/Shun'sfolderfolder/Scripts/BreakMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //parentGameObjectは親要素のgameObject
+        foreach (Transform child in gameObject.transform)
+        {
+            child.gameObject.transform.Translate(child.transform.position.x / 5.0f, child.transform.position.y / 5.0f, 0);
+        }
+
+    }
+}
