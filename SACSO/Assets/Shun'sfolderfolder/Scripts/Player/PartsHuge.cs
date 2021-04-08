9c8882e72da8a24015e8ecb0f6cb7fe512eb1c20
Shun_Nakada <shun-nakada-19h@stu.kbc.ac.jp>
2021-04-08 13:26:11 +0900

    体の部位を大きくするスクリプトとフォルダーの整理

---
+++ SACSO/Assets/Shun'sfolderfolder/Scripts/Player/PartsHuge.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartsHuge : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] private Transform RightArm;
+    [SerializeField] private Transform LefttArm;
+
+    private float RightHugeRate, LeftHugeRate;
+
+    private static float HugeRate = 0.2f;
+    private static float SmallRate = 0.1f;
+
+    Vector3 StartScale = new Vector3(1f, 1f, 1f);
+    Vector3 LastScale = new Vector3(2f, 1.5f, 3f);
+
+
+    void Start()
+    {
+        RightHugeRate = 0f;
+        LeftHugeRate = 0f;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HugeTest();
+
+        RightArm.localScale = Vector3.Lerp(StartScale, LastScale, RightHugeRate);
+        LefttArm.localScale = Vector3.Lerp(StartScale, LastScale, LeftHugeRate);
+    }
+
+    public void HugeRight()
+    {
+        if(RightHugeRate <= 1f)
+        {
+            RightHugeRate += HugeRate;
+        }
+    }
+
+    public void SmallRight()
+    {
+        if (RightHugeRate >= 0f)
+        {
+            RightHugeRate -= SmallRate;
+        }
+    }
+
+    public void HugeLeft()
+    {
+        if (LeftHugeRate <= 1f)
+        {
+            LeftHugeRate += HugeRate;
+        }
+    }
+
+    public void SmallLeft()
+    {
+        if (LeftHugeRate <= 0f)
+        {
+            LeftHugeRate -= SmallRate;
+        }
+    }
+
+
+
+    /*テスト関数*/
+    void HugeTest()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            HugeRight();
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            SmallRight();
+        }
+    }
+}
