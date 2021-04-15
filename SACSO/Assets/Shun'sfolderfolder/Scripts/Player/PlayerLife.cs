dc68c080fab57e6449ad5f9a46252498e2dcd7a0
Shun_Nakada <shun-nakada-19h@stu.kbc.ac.jp>
2021-04-15 18:54:55 +0900

    ＊＊＊＊

--- SACSO/Assets/Shun'sfolderfolder/Scripts/Player/PlayerLife.cs
+++ SACSO/Assets/Shun'sfolderfolder/Scripts/Player/PlayerLife.cs
@@ -19,10 +19,6 @@ public class PlayerLife : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        if (transform.position.z != 0)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, 0);
-        }
     }
 
     // Update is called once per frame
