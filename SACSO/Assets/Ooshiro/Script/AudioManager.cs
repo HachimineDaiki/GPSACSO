4270469a99be013cbe1d526bb8971ffda0e79d2f
DRQE <https://github.com/DRQE/mamimumema>
2021-04-15 21:52:09 +0900

    音修正

--- SACSO/Assets/Ooshiro/Script/AudioManager.cs
+++ SACSO/Assets/Ooshiro/Script/AudioManager.cs
@@ -9,8 +9,17 @@ public class AudioManager : MonoBehaviour
     public AudioClip sound2;
     public AudioClip sound3;
     public AudioClip sound4;
+    public AudioClip sound5;
+    public AudioClip sound6;
 
     AudioSource audioSource;
+
+    GameObject movesound;
+    GameObject expsound;
+    Player player;
+    enemyMove enemymove;
+
+    private bool sound3flg;
 
     int WaitTime;
         //WaitTimeR;
@@ -19,6 +28,10 @@ public class AudioManager : MonoBehaviour
     {
         //Componentを取得
         audioSource = GetComponent<AudioSource>();
+        movesound = GameObject.Find("musslepants2Unity");
+        //expsound = GameObject.Find("enemy2.0");
+        player = movesound.GetComponent<Player>();
+        //enemymove = expsound.GetComponent<enemyMove>();
 
         WaitTime = 0;
         //WaitTimeR = 0;
@@ -26,6 +39,7 @@ public class AudioManager : MonoBehaviour
 
     void Update()
     {
+
         if (WaitTime > 0) WaitTime--;
         //if (WaitTimeR > 0) WaitTimeR--;
         // 攻撃音
@@ -53,11 +67,29 @@ public class AudioManager : MonoBehaviour
                 audioSource.PlayOneShot(sound2);
                 WaitTime = 130;
             }
-
             //左右移動音
             if (Input.GetKeyDown("left")) audioSource.PlayOneShot(sound3);
 
             if (Input.GetKeyDown("right")) audioSource.PlayOneShot(sound3);
         }
+        if(sound3flg == false)
+        {
+            if (player.MoveFlg == true) audioSource.PlayOneShot(sound3);
+            sound3flg = true;
+        }
+        if (player.MoveFlg == false) sound3flg = false;
+
+        //if (enemymove.KillFlg == true)
+        //{
+        //    audioSource.PlayOneShot(sound4);
+        //    Invoke("expsoundplay", 1.5f);
+        //}
     }
+
+
+
+    //private void expsoundplay()
+    //{
+    //    audioSource.PlayOneShot(sound1);
+    //}
 }
