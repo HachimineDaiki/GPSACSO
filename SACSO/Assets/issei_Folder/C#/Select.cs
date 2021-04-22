f2e2b4481011facede33b966a73dca3f6ff961de
Mkthjm <issei-higa-19f@stu.kbc.ac.jp>
2021-04-22 13:43:49 +0900

    no message

---
+++ SACSO/Assets/issei_Folder/C#/Select.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Select : MonoBehaviour
+{
+    PauseScript pausescript;
+    GameObject gamemanager;
+
+    [SerializeField] private GameObject pauseUI;
+
+    [SerializeField] private Button Return_To_Game;
+    [SerializeField] private Button Return_To_Title;
+  //  [SerializeField] private Button Volume_adjustment;
+    [SerializeField] private Button End_The_Game;
+
+    private void Start()
+    {
+        Return_To_Game.onClick.AddListener(ReturnGame);
+        Return_To_Title.onClick.AddListener(ReturnTitle);
+       // Volume_adjustment.onClick.AddListener(Volume);
+        End_The_Game.onClick.AddListener(EndGame);
+
+
+        gamemanager = GameObject.Find("GameManeger");
+        pausescript = gamemanager.GetComponent<PauseScript>();
+    }
+
+    private void ReturnGame()
+    {
+        Time.timeScale = 1f;
+        pausescript.pauseflg = false;
+        pauseUI.SetActive(!pauseUI.activeSelf);
+    }
+
+    private void ReturnTitle()
+    {
+        Time.timeScale = 1f;
+        pauseUI.SetActive(!pauseUI.activeSelf);
+        SceneManager.LoadScene("TitleScene");
+    }
+
+    private void Volume()
+    {
+
+    }
+
+    private void EndGame()
+    {
+
+       #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;   // UnityEditorの実行を停止する処理
+       #else
+        Application.Quit();                                // ゲームを終了する処理
+       #endif
+    }
+
+
+}
---
+++ SACSO/Assets/issei_Folder/C#/SelectButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//UI使うときに必要
+using UnityEngine.UI;
+
+public class SelectButton : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pauseUI;
+
+    Button button;
+
+    bool isCalledOnce = false;
+
+    void Update()
+    {
+        if (pauseUI.activeSelf)
+        {
+            if (isCalledOnce == false)
+            {
+                button = GameObject.Find("Canvas/Panel/SelectButton/Return_To_Game").GetComponent<Button>();
+                //ボタンが選択された状態になる
+                button.Select();
+                isCalledOnce = true;
+            }
+
+        }
+
+        if (pauseUI.activeSelf == false && isCalledOnce == true)
+        {
+            isCalledOnce = false;
+        }
+    }
+}
