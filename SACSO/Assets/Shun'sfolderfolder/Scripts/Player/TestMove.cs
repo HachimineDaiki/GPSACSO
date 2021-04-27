d6fbc1812bffd5f2966ac6c01d8ef47ae94147c9
Shun_Nakada <shun-nakada-19h@stu.kbc.ac.jp>
2021-04-27 13:53:27 +0900

    ステージ

---
+++ SACSO/Assets/Shun'sfolderfolder/Scripts/Player/TestMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TestMove : MonoBehaviour
+{
+    public Transform[] points;          //巡回するポイント
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+    public float chaspeed = 15f;        //プレイヤーのスピード
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+        GotoNextPoint();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (agent.remainingDistance < 0.5f)
+        {
+            GotoNextPoint();
+        }
+    }
+
+
+    void GotoNextPoint()
+    {
+        if (points.Length == 0)return;
+        agent.destination = points[destPoint].position;
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+    public void EneChasing()
+    {
+        transform.position += transform.forward * chaspeed;
+    }
+}
