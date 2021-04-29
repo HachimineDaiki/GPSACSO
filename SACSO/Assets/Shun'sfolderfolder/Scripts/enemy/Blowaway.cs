cd4be932be63c13ca495e90a5d3689e61f5f1918
Shun_Nakada <shun-nakada-19h@stu.kbc.ac.jp>
2021-04-29 15:37:27 +0900

    stageと雑魚敵

---
+++ SACSO/Assets/Shun'sfolderfolder/Scripts/enemy/Blowaway.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blowaway : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    [SerializeField] GameObject explosion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = transform.GetComponent<Rigidbody>();
+
+        rb.AddForce(transform.forward * -100, ForceMode.Impulse);
+        rb.AddTorque(transform.forward * 100);
+        Invoke("ExplosionSet", 1.5f);
+        Destroy(gameObject, 1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ExplosionSet()
+    {
+        Instantiate(explosion, this.transform.position, Quaternion.identity);
+        //gameObject.SetActive(false);
+        Destroy(gameObject, 2.0f);
+    }
+}
