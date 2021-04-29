eaec6264ecda430c368ed77eaef1ca481b42b774
Mkthjm <issei-higa-19f@stu.kbc.ac.jp>
2021-04-29 14:00:21 +0900

    プレイヤー移動

---
+++ SACSO/Assets/issei_Folder/C#/Move.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    [SerializeField] float Speed = 50.0f;
+
+    private void Update()
+    {
+        Vector3 velocity = gameObject.transform.forward * Speed;
+        gameObject.transform.position += velocity * Time.deltaTime;
+
+    }
+}
