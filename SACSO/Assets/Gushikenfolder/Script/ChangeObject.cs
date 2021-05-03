70fab720a5e4d7963000083949eaa86c4cda0e0e
GushikenKaito <kaito-gushiken19h@stu.kbc.ac.jp>
2021-05-03 21:58:26 +0900

    ドラムのオブジェクトと切り替えるスクリプトをプッシュ

---
+++ SACSO/Assets/Gushikenfolder/Script/ChangeObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeObject : MonoBehaviour
+{
+    public GameObject Drum;
+    public GameObject BreakDrum;
+
+    private void Start()
+    {
+        Drum.SetActive(false);
+        BreakDrum.SetActive(true);
+    }
+}
