09e116a48ad8e159ee49b70aecf9ad9afa39b10c
akado_ran <nisihuni1234@gmail.com>
2021-05-06 11:35:05 +0900

    クリアシーンの値引継ぎ　後は画像とマッスルだけっす・・・

--- SACSO/Assets/Hachi'sFloder/Catinp/MuscleScript/partsend.cs
+++ SACSO/Assets/Hachi'sFloder/Catinp/MuscleScript/partsend.cs
@@ -1,12 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class partsend : MonoBehaviour
 {
 
     private int rightpoint;
     private int leftpoint;
+    private Text rightPointText;
+    private Text leftPointText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +16,22 @@ public class partsend : MonoBehaviour
         leftpoint = PlayerPrefs.GetInt("lpumpup",0);
         Debug.Log(rightpoint);
         Debug.Log(leftpoint);
+
+        //テキスト取得
+        rightPointText = GameObject.Find("RightPoint").GetComponent<Text>();
+        leftPointText = GameObject.Find("LeftPoint").GetComponent<Text>();
+
+        //仮％変換(筋肉の増加量が明確になったら修正して）
+        rightpoint *= 50;
+        leftpoint  *= 50;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //右
+        rightPointText.text = "Right :" + rightpoint.ToString()+"%";
+        //左
+        leftPointText.text = "Left :" + leftpoint.ToString() +"%";
     }
 }
---
+++ SACSO/Assets/Hodaka/ClearScore.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClearScore : MonoBehaviour
+{
+    private int scorePoint;//スコア用変数
+    private Text scorePointText;//スコアのテキスト
+    private int rightpoint;
+    private int leftpoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //スコアのテキスト取得
+        scorePointText = GameObject.Find("ClearScore").GetComponent<Text>();
+        //通常スコア
+        scorePoint = PlayerPrefs.GetInt("Score", 0);
+        //筋肉のスコア
+        rightpoint = PlayerPrefs.GetInt("rpumpup", 0)*500;
+        leftpoint = PlayerPrefs.GetInt("lpumpup", 0)*500;
+
+        scorePoint += rightpoint + leftpoint;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scorePointText.text = "SCORE :"+scorePoint.ToString("000000");
+    }
+}
