ddbd96b59fbc6d7a09d31a3263b98f90aa426b78
Shun_Nakada <shun-nakada-19h@stu.kbc.ac.jp>
2021-05-10 14:18:24 +0900

    無敵時間を作りたいなぁ

--- SACSO/Assets/Shun'sfolderfolder/Scripts/Player/PlayerLife.cs
+++ SACSO/Assets/Shun'sfolderfolder/Scripts/Player/PlayerLife.cs
@@ -8,6 +8,8 @@ public class PlayerLife : MonoBehaviour
 {
     public int Life;
     [SerializeField] private const int MaxLife = 3;
+    private bool muteki;
+    private float mutekiTime;
 
     private ScoreCon SC;//ScoreCon.scの参照　こいつの書き方が分からないので大文字にしました　許してください
 
@@ -16,9 +18,11 @@ public class PlayerLife : MonoBehaviour
     {
         SC = GameObject.Find("GameManeger").GetComponent<ScoreCon>();
         Life = MaxLife;
+        muteki = false;
     }
     private void FixedUpdate()
     {
+        MutekiCon();
     }
 
     // Update is called once per frame
@@ -26,12 +30,26 @@ public class PlayerLife : MonoBehaviour
     public void Damege()
     {
         Life--;
+
+        //ヒットすると無敵時間を設定
+        muteki = true;
+        mutekiTime = 2.0f;
+
         if (Life <= 0)
         {
             //スコアを記録する
             SC.ScoreSave();
             //ゲームオーバーへ
             SceneManager.LoadScene("GameOver");
+        }
+    }
+
+    private void MutekiCon()
+    {
+        if(mutekiTime > 0)
+        {
+            mutekiTime -= Time.deltaTime;
+            if (mutekiTime <= 0) muteki = false;
         }
     }
 }
