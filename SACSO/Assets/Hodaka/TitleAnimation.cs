8fd8d6570312a4691851d7b6428a47058cabaaab
akado_ran <nisihuni1234@gmail.com>
2021-05-12 17:34:26 +0900

    タイトルシーンの修正

---
+++ SACSO/Assets/Hodaka/TitleAnimation.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Animations;
+
+public class TitleAnimation : MonoBehaviour
+{
+    [SerializeField] Canvas canvas;//canvasの取得
+    [SerializeField] Canvas animationCanvas;//Animationcanvasの取得
+    [SerializeField] GameObject titleRed;
+    [SerializeField] Image titleBlue;
+    [SerializeField] Image titleGreen;
+    [SerializeField] Image titleMusleRunRogo;
+    private float elapsedTime;
+    private string elapsedAnimation;
+    Animation RedAnim;
+    Animation GreenAnim;
+    Animation BlueAnim;
+    Animation TitleAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.gameObject.SetActive(false);//canvasの非表示
+        animationCanvas.gameObject.SetActive(true);//animationCanvasを表示
+
+        //アニメーションの取得
+        RedAnim = titleRed.GetComponent<Animation>();
+        GreenAnim = titleGreen.GetComponent<Animation>();
+        BlueAnim = titleBlue.GetComponent<Animation>();
+        TitleAnim = titleMusleRunRogo.GetComponent<Animation>();
+
+        elapsedAnimation = "Green";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;//経過時間
+        Animation();
+    }
+
+    void Animation()
+    {
+        //elapsedTime
+        if (elapsedTime > 2.0 && elapsedAnimation == "End")
+        {
+            canvasActiv();
+        }
+
+        if (elapsedTime > 0 && elapsedAnimation == "Green")
+        {
+            GreenAnim.Play();
+            elapsedAnimation = "Blue";
+        }
+        else if (elapsedTime > 0.5 && elapsedAnimation == "Blue")
+        {
+            BlueAnim.Play();
+            elapsedAnimation = "Red";
+        }
+        else if (elapsedTime > 1 && elapsedAnimation == "Red")
+        {
+            RedAnim.Play();
+            elapsedAnimation = "Title";
+        }
+        else if (elapsedTime > 1.5 && elapsedAnimation == "Title")
+        {
+            TitleAnim.Play();
+            elapsedAnimation = "End";
+        }
+    }
+
+    void canvasActiv()//canvasをアクティブにする
+    {
+        canvas.gameObject.SetActive(true);//canvasの表示
+        animationCanvas.gameObject.SetActive(false);//animationCanvasの非表示
+    }
+}
--- SACSO/Assets/Shun'sfolderfolder/Scripts/TitleCon.cs
+++ SACSO/Assets/Shun'sfolderfolder/Scripts/TitleCon.cs
@@ -5,16 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class TitleCon : MonoBehaviour
 {
+    Canvas canvas;//キャンバスの取得
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-
+        canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKeyDown)
+        if (Input.anyKeyDown && canvas.gameObject.activeSelf == true)
         {
             //メインへ
             SceneManager.LoadScene("Main");
