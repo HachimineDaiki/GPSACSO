5b42967ecdade92f1657f90d3f7363b330dc0393
Shun_Nakada <shun-nakada-19h@stu.kbc.ac.jp>
2021-05-20 03:12:28 +0900

    オープニングシーンのテキストの追加

---
+++ SACSO/Assets/Shun'sfolderfolder/Scripts/StoryText.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryText : MonoBehaviour
+{
+    [SerializeField] List<string> messageList = new List<string>();//会話文リスト
+    [SerializeField] Text text;
+    [SerializeField] float novelSpeed;//一文字一文字の表示する速さ
+    [SerializeField] float NextLineTime;  //次の行に進む時間
+    int novelListIndex = 0; //現在表示中の会話文の配列
+    private float time;
+    private bool LineComp;
+
+
+    void Start()
+    {
+
+        LineComp = false;
+        StartCoroutine(Novel());
+    }
+
+    private void FixedUpdate()
+    {
+        if (LineComp)
+        {
+            NextLine();
+        }
+    }
+
+
+    private IEnumerator Novel()
+    {
+        int messageCount = 0; //現在表示中の文字数
+        text.text = ""; //テキストのリセット
+        while (messageList[novelListIndex].Length > messageCount)//文字をすべて表示していない場合ループ
+        {
+            text.text += messageList[novelListIndex][messageCount];//一文字追加
+            messageCount++;//現在の文字数
+            yield return new WaitForSeconds(novelSpeed);//任意の時間待つ
+        }
+
+        novelListIndex++; //次の会話文配列
+        if (novelListIndex < messageList.Count)//全ての会話を表示したか
+        {
+            time = Time.time;
+            LineComp = true;
+        }
+    }
+
+    void NextLine()
+    {
+        if ((Time.time - time) >= NextLineTime)
+        {
+            StartCoroutine(Novel());
+            LineComp = false;
+        }
+
+    }
+}
